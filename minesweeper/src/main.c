7ce5e7ca8ee286e070893ea4eab052142ecaab31
Juraj Motuz <motuzjuraj@gmail.com>
2024-01-12 17:17:52 +0100

    minesweeper: added -h (help) option

--- minesweeper/src/main.c
+++ minesweeper/src/main.c
@@ -27,29 +27,28 @@ State gameState = PLAYING;
 
 int main(int argc, char *argv[]) {
     int opt;
-    while ((opt = getopt(argc, argv, "s:mw:h:")) != -1) {
+    while ((opt = getopt(argc, argv, "s:mr:c:h")) != -1) {
         switch (opt) {
-            case 's': {
+            case 's':
                 custom_seed = true;
                 arg_set_seed(optarg);
                 break;
-            }
-            case 'm': {
+            case 'm':
                 minimal = true;
                 break;
-            }
-            case 'w': {
+            case 'c': // width
                 if (w == 0) {
                     w = atoi(optarg);
                 }
                 break;
-            }
-            case 'h': {
+            case 'r': // height
                 if (h == 0) {
                     h = atoi(optarg);
                 }
                 break;
-            }
+            case 'h':
+                printf("Usage: %s [OPTION]...\n\n -r \033[3m<num>\033[0m\tNumber of rows\n -c \033[3m<num>\033[0m\tNumber of columns\n -s \033[3m<str>\033[0m\tSet a seed in alphanumerical format with '#', eg. 10x10n13c0x0#65A1549F\n -m\t\tEnables minimal mode (everything except game board is hidden)\n -h\t\tPrints this help message and exit\n", argv[0]);
+                exit(EXIT_SUCCESS);
         }
     }
 
