b5baa2058db26923125e98a5487dcc5bda1af412
Juraj Motuz <motuzjuraj@gmail.com>
2024-02-18 01:26:05 +0100

    trivia: colors, refactor, questions are now structs

--- trivia/src/main.c
+++ trivia/src/main.c
@@ -11,13 +11,19 @@
 #include "extern/b64.c/b64.h"
 #include "main.h"
 
+#define FORMAT_RESET "\033[0m"
+#define FORMAT_BOLD "\033[1m"
+#define FORMAT_UNDERLINE "\033[4m"
+#define FORMAT_CORRECT "\033[32m"
+#define FORMAT_WRONG "\033[31m"
+
 // default
 Arguments args = {
     .amount = 10,
     .category = 0,
     .difficulty = "",
-    .type = "multiple",
-    .formatting = false
+    .type = "",
+    .formatting = true
 };
 
 int process_args(int argc, char *const argv[]) {
@@ -55,11 +61,82 @@ int process_args(int argc, char *const argv[]) {
                 printf("TODO\n");
                 break;
             case 'f':
-                printf("TODO\n");
+                args.formatting = false;
                 break;
         }
     }
     return 0;
+}
+
+// print question with answers and returns correct answer number (from 0)
+Question get_struct_question(const cJSON *result, bool *err) {
+    init_question(new_question);
+
+    // write type to Question struct
+    cJSON *json_type = cJSON_GetObjectItemCaseSensitive(result, "question");
+    if (cJSON_IsString(json_type)) {
+        new_question.type = (char *)decode(json_type);
+    } else {
+        *err = true;
+        fprintf(stderr, "Error: The question type was expected to be a string.\n");
+        return new_question;
+    }
+
+    // write difficulty to Question struct
+    cJSON *json_difficulty = cJSON_GetObjectItemCaseSensitive(result, "difficulty");
+    if (cJSON_IsString(json_difficulty)) {
+        new_question.difficulty = (char *)decode(json_difficulty);
+    } else {
+        *err = true;
+        fprintf(stderr, "Error: The question difficulty was expected to be a string.\n");
+        return new_question;
+    }
+
+    // write category to Question struct
+    cJSON *json_category = cJSON_GetObjectItemCaseSensitive(result, "category");
+    if (cJSON_IsString(json_category)) {
+        new_question.category = (char *)decode(json_category);
+    } else {
+        *err = true;
+        fprintf(stderr, "Error: The question category was expected to be a string.\n");
+        return new_question;
+    }
+
+    // write question text to Question struct
+    cJSON *json_question_text = cJSON_GetObjectItemCaseSensitive(result, "question");
+    if (cJSON_IsString(json_question_text)) {
+        new_question.question_text = (char *)decode(json_question_text);
+    } else {
+        *err = true;
+        fprintf(stderr, "Error: The question text was expected to be a string.\n");
+        return new_question;
+    }
+
+    // write answers to Question struct
+    cJSON *correct_answer = cJSON_GetObjectItemCaseSensitive(result, "correct_answer");
+    cJSON *incorrect_answers = cJSON_GetObjectItemCaseSensitive(result, "incorrect_answers");
+
+    new_question.answers_amount = cJSON_GetArraySize(incorrect_answers) + 1;
+    new_question.answers = (char **)malloc(new_question.answers_amount * sizeof(char *));
+    if (new_question.answers == NULL) {
+        *err = true;
+        fprintf(stderr, "Error: Failed to allocate memory.\n");
+        return new_question;
+    }
+    
+    // store incorrect answers
+    for (int i = 0; i < new_question.answers_amount - 1; i++) {
+        cJSON *curr_incor_answer = cJSON_GetArrayItem(incorrect_answers, i);
+        new_question.answers[i] = (char *)decode(curr_incor_answer);
+    }
+
+    // randomly place correct answer
+    int random = rand() % new_question.answers_amount;
+    new_question.corr_answ_index = random;
+    new_question.answers[new_question.answers_amount - 1] = new_question.answers[random];
+    new_question.answers[random] = (char *)decode(correct_answer);
+
+    return new_question;
 }
 
 char *create_url() {
@@ -85,58 +162,47 @@ char *create_url() {
 }
 
 int process_json(cJSON *json) {
+    questions = (Question *)malloc(args.amount * sizeof(Question));
+    if (questions == NULL) {
+        return 1;
+    }
+
     cJSON *results = cJSON_GetObjectItemCaseSensitive(json, "results");
     const cJSON *result = NULL;
+    int n = 0;
     cJSON_ArrayForEach(result, results) {
-        cJSON *question = cJSON_GetObjectItemCaseSensitive(result, "question");
-        cJSON *correct_answer = cJSON_GetObjectItemCaseSensitive(result, "correct_answer");
-        cJSON *incorrect_answers = cJSON_GetObjectItemCaseSensitive(result, "incorrect_answers");
-
-        // print the question
-        if (cJSON_IsString(question)) printf("Question: %s\n", decode(question));
-
-        printf("Options: ");
-        int n = cJSON_GetArraySize(incorrect_answers) + 1; // number of all answers
-        char **answers = (char **)malloc(n * sizeof(char *));
-        if (answers == NULL) {
-            fprintf(stderr, "Error: Failed to allocate memory.\n");
-            return 1;
-        }
-        
-        // store incorrect answers
-        for (int i = 0; i < n - 1; i++) {
-            cJSON *curr_incor_answer = cJSON_GetArrayItem(incorrect_answers, i);
-            answers[i] = decode(curr_incor_answer);
-        }
-
-        // randomly place correct answer
-        int random = rand() % n;
-        answers[n - 1] = answers[random];
-        answers[random] = decode(correct_answer);
-
-        // print all avaible options (answers)
-        for (int i = 0; i < n; i++) {
-            printf("%c) %s%s", 'a' + i, answers[i], (n - 1 != i) ? ", " : "\n");
-        }
-
-        // get user input
-        char input[255];
-        printf("Your answer: ");
-        scanf("%255s", input);
-
-        // check input with the correct answer
-        if (input[0] - 'a' == random) {
-            printf("Correct!\n");
-        }
-        printf("\n");
-
-        // free all answers
-        for (int i = 0; i < n; i++) {
-            free(answers[i]);
-        }
-        free(answers);
+        bool err = false;
+        questions[n] = get_struct_question(result, &err);
+        if (err) return 1;
+        n++;
     }
     return 0;
+}
+
+int print_question(Question question) {
+    // print the question
+    printf("Q: %s%s%s\n", args.formatting ? FORMAT_BOLD : "", question.question_text, args.formatting ? FORMAT_RESET : "");
+
+    // print all avaible options (answers)
+    for (int i = 0; i < question.answers_amount; i++) {
+        printf("%c) %s%s%s%s", 'a' + i, args.formatting ? FORMAT_UNDERLINE : "", question.answers[i], args.formatting ? FORMAT_RESET : "", (question.answers_amount - 1 != i) ? ", " : "\n");
+    }
+    printf("\n");
+    return 0;
+}
+
+bool check_user_answer(Question question, char user_input) {
+    if (user_input == question.corr_answ_index + 'a') {
+        return true;
+    }
+    return false;
+}
+
+char get_input() {
+    char input[2];
+    printf("Your answer: ");
+    scanf("%s", input);
+    return input[0];
 }
 
 int main(int argc, char *argv[]) {
@@ -145,9 +211,9 @@ int main(int argc, char *argv[]) {
     process_args(argc, argv);
 
     // load json from url
-    
     char *url = create_url();
     char *json_data = get_json(url);
+    free(url);
     
     // parse json
     cJSON *json = cJSON_Parse(json_data);
@@ -163,6 +229,20 @@ int main(int argc, char *argv[]) {
     if (process_json(json)) return 1;
     
     cJSON_Delete(json);
-    free(url);
+
+    for (int i = 0; i < args.amount; i++) {
+        print_question(questions[i]);
+        char input = get_input();
+        if (check_user_answer(questions[i], input)) {
+            questions[i].correctly_answered = true;
+        }
+
+        // check input with the correct answer
+        if (questions[i].correctly_answered) {
+            printf("%sCorrect!%s\n\n", args.formatting ? FORMAT_CORRECT : "", args.formatting ? FORMAT_RESET : "");
+        } else {
+            printf("%sWrong, correct answer was: %c) %s%s%s\n\n", args.formatting ? FORMAT_WRONG : "", questions[i].corr_answ_index + 'a', args.formatting ? FORMAT_UNDERLINE : "", questions[i].answers[questions[i].corr_answ_index], args.formatting ? FORMAT_RESET : "");
+        }
+    }
     return 0;
 }
--- trivia/src/main.h
+++ trivia/src/main.h
@@ -4,12 +4,28 @@
 #define decode(x) b64_decode(x->valuestring, strlen(x->valuestring))
 #define uint8_t unsigned char
 
-typedef struct {
+typedef struct Arguments{
     uint8_t amount; // number of questions
     uint8_t category; // id of categories, 0 is all categories
     char difficulty[7]; // difficulty of questions - easy / medium / hard, 0 is all difficulties
     char type[9]; // type of question - multiple / boolean, 0 is all types
     bool formatting; // use formatting / colors
 } Arguments;
+
+typedef struct Question{
+    uint8_t index; // index of the question
+    char *type; // type of the question; multiple / boolean
+    char *difficulty; // difficulty of the question; easy / medium / hard
+    char *category; // category of the question
+    char *question_text; // question asked
+    char **answers; // all answers including correct and incorrect ones
+    uint8_t answers_amount; // amount of answers
+    uint8_t corr_answ_index; // index number for the correct answer in 'answers'
+    bool correctly_answered; // true if user answered question correctly, default is false
+} Question;
+
+#define init_question(x) Question x = { .index = 0, .type = NULL, .difficulty = NULL, .category = NULL, .question_text = NULL, .answers = NULL, .answers_amount = 0, .corr_answ_index = 0, .correctly_answered = false }
+
+Question *questions;
 
 #endif
